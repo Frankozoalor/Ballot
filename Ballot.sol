22cc76b17b65de7f580172494859244f16e9c0b1
EkeneOzoalor <88558745+EkeneOzoalor@users.noreply.github.com>
2022-04-15 15:04:58 -0700

    new chnages

--- Ballot.sol
+++ Ballot.sol
@@ -24,7 +24,7 @@
         uint256 public startTime = block.timestamp;
         
         modifier voteEnded() {
-            require(block.timestamp < startTime + 2 minutes, "Vote has Ended");
+            require(block.timestamp < startTime + 2 minutes, "Vote Ended");
             _;
         }
 
